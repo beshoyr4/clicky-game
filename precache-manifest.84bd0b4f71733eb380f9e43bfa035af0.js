5bd8aabb57f4bcc6108c803fb4c0ad31a78eec25
beshoyr4 <33008284+beshoyr4@users.noreply.github.com>
2019-04-23 21:48:51 -0500

    Updates

---
+++ precache-manifest.84bd0b4f71733eb380f9e43bfa035af0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ebc47207f97cae647feded99e962c60",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bc43f6e1896759fe4db4",
+    "url": "/static/css/main.381f2756.chunk.css"
+  },
+  {
+    "revision": "dd0ed99a380f8d225736",
+    "url": "/static/js/2.884c28c3.chunk.js"
+  },
+  {
+    "revision": "bc43f6e1896759fe4db4",
+    "url": "/static/js/main.ce0a99f6.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
